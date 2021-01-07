f47186b13aa4c95fbb66165c63e668a681e1577e
YogiPatel8055 <76836330+YogiPatel8055@users.noreply.github.com>
2021-01-07 14:50:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixrect, movingrect;
+function setup() {
+createCanvas(1200,800);
+fixrect=createSprite(600, 400, 90, 50);
+fixrect.shapeColor="green";
+fixrect.debug=true;
+
+movingrect=createSprite(400,600,50,90);
+movingrect.shapeColor="green";
+movingrect.debug=true;
+}
+
+function draw() {
+  background(0);  
+
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+
+
+
+  if(movingrect.x-fixrect.x<fixrect.width/2+movingrect.width/2&&
+    fixrect.x-movingrect.x<fixrect.width/2+movingrect.width/2&&
+    movingrect.y-fixrect.y<fixrect.height/2+movingrect.height/2&&
+    fixrect.y-movingrect.y<fixrect.height/2+movingrect.height/2){
+    movingrect.shapeColor="red";
+    fixrect.shapeColor="red";
+  }else
+  { movingrect.shapeColor="green";
+    fixrect.shapeColor="green";
+  }
+  drawSprites();
+}
